b372e7f25e800bae406d8ec059c58a7401f1670a
Arran Lemon <arran.t.lemon@gmail.com>
2021-09-02 16:33:30 +1000

    implement hit sit and is_bust fns and fix get values bug

--- Blackjack.py
+++ Blackjack.py
@@ -1,44 +1,87 @@
 
 import random
 
-deck = [["2H",2], ["3H",3], ["4H",4], ["5H",5], ["6H",6], ["7H",7], ["8H",8], ["9H",9], ["10H",10], ["JH",10], ["QH",10], ["KH",10], ["AH",11,1], ["2S",2], ["3S",3], ["4S",4], ["5S",5], ["6S",6], ["7S",7], ["8S",8], ["9S",9], ["10S",10], ["JS",10], ["QS",10], ["KS",10], ["AS",11,1], ["2D",2], ["3D",3], ["4D",4], ["5D",5], ["6D",6], ["7D",7], ["8D",8], ["9D",9], ["10D",10], ["JD",10], ["QD",10], ["KD",10], ["AD",11,1], ["2C",2], ["3C",3], ["4C",4], ["5C",5], ["6C",6], ["7C",7], ["8C",8], ["9C",9], ["10C",10], ["JC",10], ["QC",10], ["KC",10], ["AC",11,1]]
+deck = [["2H",2], ["3H",3], ["4H",4], ["5H",5], ["6H",6], ["7H",7], ["8H",8], ["9H",9], ["10H",10], ["JH",10], ["QH",10], ["KH",10], ["AH",1,11], ["2S",2], ["3S",3], ["4S",4], ["5S",5], ["6S",6], ["7S",7], ["8S",8], ["9S",9], ["10S",10], ["JS",10], ["QS",10], ["KS",10], ["AS",1,11], ["2D",2], ["3D",3], ["4D",4], ["5D",5], ["6D",6], ["7D",7], ["8D",8], ["9D",9], ["10D",10], ["JD",10], ["QD",10], ["KD",10], ["AD",1,11], ["2C",2], ["3C",3], ["4C",4], ["5C",5], ["6C",6], ["7C",7], ["8C",8], ["9C",9], ["10C",10], ["JC",10], ["QC",10], ["KC",10], ["AC",1,11]]
+
 
 class Hand:
     
     def __init__(self, cards, money_bet = 0):
         self.cards = cards
-        self.hand_values = []
+        self.hand_values = [0]
         self.money_bet = money_bet
         self.money_made = 0
         self.best_hand_value = 0
+
+    def __repr__(self):
+        cards = ""
+        for card in self.cards:
+            cards += card[0] + " "
+        self.get_value()
+        best_current_value = self.get_best_value()
+        return "Your cards:\n" + cards + "\nbest current value: " + str(self.best_hand_value)
 
     def get_value(self):
         values = [0]
         for card in self.cards:
             card_values = card[1:]
-            print(card_values)
             for index in range(0,len(values)):
                 values[index] += card_values[0]
                 if len(card_values) > 1:
-                    values.append(values[index] + card_values[1])
-        print(values)
-        return values            
+                    values.append(values[index]-1 + card_values[1])
+        self.hand_values = values
+        return            
 
 
-def get_best_value(values):
-        best_value = 0
-        for index in range(0,len(values)):
-            if index == 0 and values[index] > 21:
-                best_value = values[index]
-                break
-            elif values[index] > best_value and values[index] <=21:
-                best_value = values[index]
-        return best_value
+    def get_best_value(self):
+            for index in range(0,len(self.hand_values)):
+                if index == 0 and self.hand_values[index] > 21:
+                    self.best_hand_value = self.hand_values[index]
+                    break
+                elif self.hand_values[index] > self.best_hand_value and self.hand_values[index] <=21:
+                    self.best_hand_value = self.hand_values[index]
+            return
 
-def get_cards():
+    def hit(self):
+        self.cards.append(get_single_card(deck))
+        print(self)
+        return 
+
+    
+    def sit(self):
+        confirmed = False
+        while confirmed == False:
+            confirmation_decision = input("Confirming that you would like to sit with this hand of current value {}?(y/n)".format(self.best_hand_value)).lower().strip()
+            if confirmation_decision == "y":
+                confirmed = True
+            elif confirmation_decision == "n":
+                print("What would you like to do with this hand?")
+                return
+            else:
+                print("Please enter y or n.")
+        return
+
+    def is_bust(self):
+        if self.best_hand_value > 21:
+            return True
+        return False
+
+
+
+def get_first_cards(deck):
         card1 = deck.pop(random.randint(0,len(deck)))
         card2 = deck.pop(random.randint(0,len(deck)))
         return [card1,card2]
+
+def get_single_card(deck):
+    card = deck.pop(random.randint(0,len(deck)))
+    return card
+
+def reset_deck(deck):
+    deck = [["2H",2], ["3H",3], ["4H",4], ["5H",5], ["6H",6], ["7H",7], ["8H",8], ["9H",9], ["10H",10], ["JH",10], ["QH",10], ["KH",10], ["AH",1,11], ["2S",2], ["3S",3], ["4S",4], ["5S",5], ["6S",6], ["7S",7], ["8S",8], ["9S",9], ["10S",10], ["JS",10], ["QS",10], ["KS",10], ["AS",1,11], ["2D",2], ["3D",3], ["4D",4], ["5D",5], ["6D",6], ["7D",7], ["8D",8], ["9D",9], ["10D",10], ["JD",10], ["QD",10], ["KD",10], ["AD",1,11], ["2C",2], ["3C",3], ["4C",4], ["5C",5], ["6C",6], ["7C",7], ["8C",8], ["9C",9], ["10C",10], ["JC",10], ["QC",10], ["KC",10], ["AC",1,11]]
+    return
+    
+
 
 class Player:
     
@@ -49,11 +92,9 @@ class Player:
         self.chips = chips
         self.hand_history = []
         self.hand_value = -1
-    
-    
 
     def play_hand(self):
-
+    
         money_bet = 0
         playing_hand = False
         while playing_hand == False:
@@ -80,7 +121,7 @@ class Player:
                     while bet_confirmed == False:
                         bet_confirmation = input("You want to bet {} chips on this hand?(y/n)".format(money_bet)).lower().strip()
                         if bet_confirmation == "y":
-                            self.currenthands = Hand(get_cards(), money_bet)
+                            self.currenthands = Hand(get_first_cards(), money_bet)
                             money_confirmed = True
                             bet_confirmed = True
                         elif bet_confirmation == "n":
@@ -93,29 +134,20 @@ class Player:
                             continue
             else:
                 print("Please enter y or n.")
-
-        print("Works till now I think")
-        return 
-
-
-
-
+        #now we actually play the hand
+        for hand in self.currenthands:
+            hand.play_hand()
+        self.get_value()
+        self.get_best_value()
         
-                
-
-
-
-        
-
-
+        actions = ["hit", "sit", "bust", "blackjack!"]
+        action = 0
     
+reset_deck(deck)      
+test_hand = Hand([["AD",1,11],["KS",10]],100)
+print(test_hand)
 
 
-    
-        
-test_player = Player("zestyboy", 100)
-test_player.play_hand()        
-# test_hand = Hand([["JH",10], ["AD",1,11], ["AS",1,11]])
 # test_values = test_hand.get_value()
 # print(test_values)
 # print(get_best_value(test_values))
